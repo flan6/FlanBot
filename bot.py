3221d5f700cddd257aee5f14492b6d59cc953363
LuigiAzevedo <luigiazevedo97@gmail.com>
2022-06-15 20:29:15 -0300

    Feature: Add Astronomy Picture of the Day bot command (#1)

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 # bot.py
+import logging
 import os
 import random
-import logging
 
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from nasa import Nasa
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+NASA_TOKEN = os.environ.get("NASA_TOKEN")
 
 logging.basicConfig(filename='flanbot.log',
                     format='%(asctime)s %(levelname)s:%(message)s',
@@ -27,9 +29,25 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     await ctx.send(', '.join(dice))
 
 
-@bot.command(name='BEEP', help='Mario.')
+@bot.command(name='BEEP', help='BOOP')
 async def luigi(ctx):
     await ctx.send('BOOP')
 
+
+@bot.command(name='nasa', help='Astronomy Picture of the Day. Type !nasa '
+                               'extra to see details ')
+async def nasa(ctx, details: str = None):
+    info = Nasa.get_nasa_picture(NASA_TOKEN)
+    if not info:
+        await ctx.send('Ocorreu um problema ao buscar a imagem do dia!')
+        return
+
+    await ctx.send(info['img'])
+    await ctx.send(f"**{info['title']}**")
+
+    if details in ['details', 'extra']:
+        await ctx.send(f"{info['explanation']}")
+
+
 if __name__ == '__main__':
-    bot.run(TOKEN)
+    bot.run(DISCORD_TOKEN)
---
+++ nasa.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class Nasa:
+
+    @staticmethod
+    def get_nasa_picture(token):
+        """ Access NASA API and retrieve the picture of the day """
+
+        # Contact API
+        try:
+            url = f"https://api.nasa.gov/planetary/apod?api_key={token}"
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException:
+            return None
+
+        # Parse response
+        try:
+            get = response.json()
+            result = {
+                'img': get["url"],
+                'title': get["title"],
+                'explanation': get["explanation"]
+            }
+
+            return result
+
+        except (KeyError, TypeError, ValueError):
+            return None
