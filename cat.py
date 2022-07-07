dccd246b89080e7877eea37e504f6124174c6499
LuigiAzevedo <luigiazevedo97@gmail.com>
2022-07-07 12:49:51 -0300

    Bot command Nasa and Cat refactor

--- cat.py
+++ cat.py
@@ -1,21 +1,13 @@
-from typing import Union
-
 import requests
 
 
-def cat_status_code(status_code: str) -> Union[None, str]:
+def cat_status_code(status_code: str) -> None | str:
     """ Request image from HTTP Cat API """
 
-    # Contact API
     try:
         url = f"https://http.cat/{status_code}.jpg"
         response = requests.get(url)
         response.raise_for_status()
-    except requests.RequestException:
-        return None
-
-    # Parse response
-    try:
         return url
-    except (KeyError, TypeError, ValueError):
+    except (requests.RequestException, KeyError, TypeError, ValueError):
         return None
--- nasa.py
+++ nasa.py
@@ -1,29 +1,21 @@
-from typing import Union
-
 import requests
 
 
 class Nasa:
 
     @staticmethod
-    def get_nasa_picture(token: str) -> Union[None, dict]:
+    def get_nasa_picture(token: str) -> None | dict:
         """ Access NASA API and retrieve the picture of the day """
 
-        # Contact API
         try:
             url = f"https://api.nasa.gov/planetary/apod?api_key={token}"
             response = requests.get(url)
             response.raise_for_status()
-        except requests.RequestException:
-            return None
-
-        # Parse response
-        try:
             get = response.json()
             return {
                 'img': get["url"],
                 'title': get["title"],
-                'explanation': get["explanation"]
+                'explanation': get["explanation"],
             }
-        except (KeyError, TypeError, ValueError):
+        except (requests.RequestException, KeyError, TypeError, ValueError):
             return None
